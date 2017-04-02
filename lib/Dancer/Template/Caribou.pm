c6b29960ba9df421476a8e69047842d7093abe9e
Yanick Champoux <yanick@babyl.dyndns.org>
2017-04-02 14:48:51 -0400

    update the documentation

--- lib/Dancer/Template/Caribou.pm
+++ lib/Dancer/Template/Caribou.pm
@@ -1,15 +1,10 @@
 package Dancer::Template::Caribou;
 #ABSTRACT: Template::Caribou wrapper for Dancer
 
-# TODO I think the template loading can be simplified
-
 use strict;
 use warnings;
 
-use Path::Tiny;
-use Path::Iterator::Rule;
 use Moose::Util qw/ with_traits find_meta /;
-use FindBin;
 use Dancer::Config qw/ setting /;
 use Module::Runtime qw/ use_module /;
 
@@ -24,7 +19,7 @@ has 'default_tmpl_ext' => (
     default => sub { 'bou' },
 );
 
-has main => (
+has default_template => (
     is => 'ro',
     lazy => 1,
     default => sub {
@@ -38,10 +33,6 @@ has namespace => (
     lazy => 1,
     predicate => 'has_namespace',
     default => sub {
-        use DDP;
-        $DB::single = 1;
-        
-        p $_[0]->config;
         $_[0]->config->{namespace} || 'Dancer::View';
     },
 );
@@ -66,8 +57,6 @@ sub layout {
 sub render {
     my( $self, $template, $tokens ) = @_;
 
-    $DB::single = 1;
-
     my $class = $template;
     $class = join '::', $self->namespace, $class
         unless $class =~ s/^\+//;
@@ -83,7 +72,7 @@ sub render {
         $class = with_traits( $class, use_module($layout_class) );
     }
 
-    my $method = $self->main;
+    my $method = $self->default_template;
     my $x = $class->new( %$tokens)->$method;
     use utf8;utf8::decode($x);
     return $x;
@@ -94,6 +83,7 @@ sub view {
     return $view;
 }
 
+# TODO check if the class exists
 sub view_exists {
     1;
 }
@@ -110,11 +100,10 @@ __END__
     template: Caribou
 
     engines:
-      template:
-        Caribou:
-          namespace:    MyApp::View
-          auto_reload:  1
-
+      Caribou:
+        namespace:    MyApp::View
+        layout_namespace: MyApp::View::Layout
+        default_template: page
 
     # and then in the application
     get '/' => sub { 
@@ -143,119 +132,45 @@ At the base, if you do
 the template name (here I<MyView>) will be concatenated with the 
 configured view namespace (which defaults to I<Dancer::View>)
 to generate the Caribou class name. A Caribou object is created
-using C<%options> as its arguments, and its inner template C<page> is then
+using C<%options> as its arguments, and its default template (defaulting to C<page>) 
+is then
 rendered. In other words, the last line of the code above becomes 
 equivalent to 
 
-    return Dancer::View::MyView->new( %options )->render('page');
-
-=head2 '/views' template classes
-
-Template classes can be created straight from the C</views> directory.
-Any directory containing a file named C<bou> will be turned into a 
-C<Template::Caribou> class. Additionally, any file with a C<.bou> extension
-contained within that directory will be turned into a inner template for 
-that class.
-
-=head3 The 'bou' file
-
-The 'bou' file holds the custom bits of the Template::Caribou class.
-
-For example, a basic welcome template could be:
-
-    # in /views/welcome/bou
-    
-    use Template::Caribou::Tags::HTML ':all';
-
-    has name => ( is => 'ro' );
-
-    template page => sub {
-        my $self = shift;
-
-        html {
-            head { title { 'My App' } };
-            body {
-                h1 { 'hello ' . $self->name .'!' };
-            };
-        }
-    };
-
-which would be invoqued via
-
-    get '/hi/:name' => sub {
-        template 'welcome' => { name => param('name') };
-    };
-
-
-=head3 The inner template files
-
-All files with a '.bou' extension found in the same directory as the 'bou'
-file become inner templates for the class. So, to continue with the example
-above, we could change it into
-
-    # in /views/howdie/bou
-    
-    use Template::Caribou::Tags::HTML ':all';
-
-    has name => ( is => 'ro' );
-
-
-    # in /views/howdie/page
-    sub {
-        my $self = shift;
-
-        html {
-            head { title { 'My App' } };
-            body {
-                h1 { 'howdie ' . $self->name . '!' };
-            };
-        }
-    }
+    return Dancer::View::MyView->new( %options )->page;
 
 =head3 Layouts as roles
 
-For the layout sub-directory, an additional piece of magic is performed.
-The 'bou'-marked directories are turned into roles instead of classes, which will be applied to
-the template class. Again, to take our example:
-
-    # in /views/layouts/main/bou
-    # empty file
-
-    # in /views/layouts/main/page
-    
-    # the import of tags really needs to be here 
-    # instead than in the 'bou' file 
-    use Template::Caribou::Tags::HTML ':all';
-
-    sub {
-        my $self = shift;
-
-        html {
-            head { title { 'My App' } };
-            body {
-                $self->inner_template;
-            };
-        }
-    }
-
-    # in /views/hullo/bou
-    
-    use Template::Caribou::Tags::HTML ':all';
-
-    has name => ( is => 'ro' );
-
-    # in /views/howdie/inner
-    sub { my $self = shift; h1 { 'hullo ' . $self->name . '!' } }
-
+Layouts, just like templates, are package names. They are expected to be
+roles that will be composed with the template class.
 
 =head1 CONFIGURATION
 
 =over
 
+=item default_template
+
+The name of the entry template to use. In other words, with the configuration
+given in the SYNOPSIS, the dancer code
+
+    return template 'MyThing';
+
+is equivalent to
+
+    return MyApp::View::MyThing->page;
+
+Defaults to C<page>.
+
 =item namespace 
 
-The namespace under which the Caribou classes are created.
+The namespace under which the Caribou template classes are.
 defaults to C<Dancer::View>.
+
+=item layout_namespace 
+
+The namespace under which the Caribou layout roles are.
+defaults to the C<::Layout> sub-namespace under the template
+namespace.
 
 =back
 
